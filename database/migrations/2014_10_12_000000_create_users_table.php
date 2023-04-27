494c0acfcde8830afb02a5483ce24957648cfee8
valT-800 <65894951+valT-800@users.noreply.github.com>
2023-04-27 12:03:11 +0300

    modified users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,18 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('level_id')->unsigned()->nullable();
+            $table->foreignId('country_id')->unsigned()->nullable();
+            $table->string('city')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('cover')->nullable();
+            $table->bigInteger('points')->nullable();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+
+            $table->foreign('level_id')->references('id')->on('levels');
+            $table->foreign('country_id')->references('id')->on('countries');
         });
     }
 
