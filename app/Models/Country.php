c9f2e4d64b4d444a10154cdf1c2dac6400977ca3
valT-800 <65894951+valT-800@users.noreply.github.com>
2023-04-27 12:02:54 +0300

    added Country model and migration

---
+++ app/Models/Country.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'countries';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'code'];
+}
