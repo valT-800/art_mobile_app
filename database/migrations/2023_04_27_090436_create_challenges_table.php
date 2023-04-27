ce37fa534cf8bcd147e6a005bea35deb6866f8b5
valT-800 <65894951+valT-800@users.noreply.github.com>
2023-04-27 12:21:22 +0300

    added Challenge model and migration

---
+++ database/migrations/2023_04_27_090436_create_challenges_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('challenges');
+    }
+};
