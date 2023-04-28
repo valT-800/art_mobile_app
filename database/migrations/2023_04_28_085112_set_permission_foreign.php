c686e952c4d6a3c6c5ba5fcd5f3940c38bda6c5d
valT-800 <65894951+valT-800@users.noreply.github.com>
2023-04-28 12:24:56 +0300

    added user roles

---
+++ database/migrations/2023_04_28_085112_set_permission_foreign.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_has_roles', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::table('user_has_permissions', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
