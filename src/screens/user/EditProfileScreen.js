bfda3d40435c622139be9b0d91f10f83e6e5ab21
valT-800 <65894951+valT-800@users.noreply.github.com>
2023-12-05 18:57:51 +0200

    get and display server errors for user update

--- src/screens/user/EditProfileScreen.js
+++ src/screens/user/EditProfileScreen.js
@@ -1,23 +1,29 @@
 
 import React, { useContext, useEffect, useState } from "react";
-import { Button, SafeAreaView, Text, TouchableHighligh, FlatList, TouchableHighlight, View, TouchableOpacity, StyleSheet, TextInput, ActivityIndicator } from "react-native";
-import { Post } from "expo-image";
+import {SafeAreaView, Text, View, TouchableOpacity, StyleSheet, ActivityIndicator } from "react-native";
+import { Image } from "expo-image";
 import {api} from "../../services/api_base";
 import { AuthContext } from "../../AuthProvider";
-import { Ionicons } from "@expo/vector-icons";
 import CustomIcon from "../../components/CustomIcon";
 import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomButton";
 
 function EditProfileScreen({navigation: {navigate, setOptions}}){
 
-  const { user, update } = useContext(AuthContext)
+  const { user, update,error } = useContext(AuthContext)
   const[loggedUser, setLoggedUser] = useState({})
   const[name, setName] = useState('')
   const[username, setUsername] = useState('')
   const[email, setEmail] = useState('')
   const [loading, setLoading] =useState(true)
-  const updateUserInfo =async()=>{
+
+  setOptions({
+    headerRight: () =>
+    <CustomIcon name='checkmark' size = {30} event={()=>updateUserInfo()}></CustomIcon>
+    });
     
+  const updateUserInfo =async()=>{
+    //console.log(name,username,email)
     update(name, username, email)
     setLoading(true)
   }
@@ -39,13 +45,10 @@ function EditProfileScreen({navigation: {navigate, setOptions}}){
         setLoading(false)
       })
       .catch(error => {
-        //console.log("Error", error.response);
+        console.log("Error", error.response);
         setLoading(false)
       })
-      setOptions({
-        headerRight: () =>
-        <CustomIcon name='checkmark' size = {30} event={()=>updateUserInfo()}></CustomIcon>
-        });    
+          
   }, [loading]);
 
     return(
@@ -53,31 +56,40 @@ function EditProfileScreen({navigation: {navigate, setOptions}}){
           {loading ? <ActivityIndicator/> :
           <View style = {{margin: 15, width: '100%', alignItems:'center'}}>
           <View style = {{alignItems:'center', padding:10}}>
-            <Post
+            <Image
             source={loggedUser.profile_photo_url}
             style={{width: 100, height: 100, borderRadius: 50}}>
-            </Post>
-            <TouchableOpacity><Text>Edit picture</Text></TouchableOpacity>
-        </View>
-            <CustomInput
-                onChangeText={text=>setName(text)}
-                placeholder="Name"
-                textContentType='name'
-                value={name}
-            />
-            <CustomInput
-                onChangeText={text=>setUsername(text)}
-                placeholder="Username"
-                textContentType='nickname'
-                value={username}
-            />
-            <CustomInput
-                onChangeText={text=>setEmail(text)}
-                placeholder="Email"
-                textContentType='emailAddress'
-                value={email}
-            />
-            </View>}
+            </Image>
+            <CustomButton title={'Upload picture'}/>
+          </View>
+          { error && error.data.name &&
+          <Text style={{ color: 'red', marginBottom: 24 }}>{error.data.name}</Text>
+          }
+          <CustomInput
+              onChangeText={setName}
+              placeholder="Name"
+              textContentType='name'
+              value={name}
+          />
+          { error && error.data.username &&
+          <Text style={{ color: 'red', marginBottom: 24 }}>{error.data.username}</Text>
+          }
+          <CustomInput
+              onChangeText={setUsername}
+              placeholder="Username"
+              textContentType='nickname'
+              value={username}
+          />
+          { error && error.data.email &&
+          <Text style={{ color: 'red', marginBottom: 24 }}>{error.data.email}</Text>
+          }
+          <CustomInput
+              onChangeText={setEmail}
+              placeholder="Email"
+              textContentType='emailAddress'
+              value={email}
+          />
+          </View>}
         </SafeAreaView>
     )
 }
